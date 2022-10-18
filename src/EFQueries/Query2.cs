00475dcaf4dfe8d3e6d235ffa32e0b0e3e5f9bc2
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-10-18 23:41:19 +0200

    Added Query2.

---
+++ src/EFQueries/Query2.cs
@@ -0,0 +1,69 @@
+﻿namespace EFQueries;
+
+public class Query2
+{
+    public static async Task RunAsync()
+    {
+        PrintSeparator("QUERY 2");
+
+        await LinqExtensionsAsync();
+        await LinqQueryAsync();
+    }
+
+    public static async Task LinqExtensionsAsync()
+    {
+        using var dbContext = new AppDbContext();
+
+        // SelectMany generates inner join
+
+        var query = dbContext.Retailers
+            .SelectMany(r => r.CustomerRetailers, (r, cr) => new
+            {
+                Id = r.Id,
+                Name = r.Name,
+                CustomerId = cr.Customer.Id,
+                CustomerName = cr.Customer.Name,
+            });
+
+        var result = await query.ToListAsync();
+
+        Print(result);
+
+        /*
+         
+      SELECT [r].[Id], [r].[Name], [c0].[Id] AS [CustomerId], [c0].[Name] AS [CustomerName]
+      FROM [Retailers] AS [r]
+      INNER JOIN [CustomerRetailers] AS [c] ON [r].[Id] = [c].[RetailerId]
+      INNER JOIN [Customers] AS [c0] ON [c].[CustomerId] = [c0].[Id]
+
+        */
+    }
+
+    public static async Task LinqQueryAsync()
+    {
+        using var dbContext = new AppDbContext();
+
+        var query = from r in dbContext.Retailers
+                    join cr in dbContext.CustomerRetailers on r.Id equals cr.RetailerId
+                    select new
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        CustomerId = cr.Customer.Id,
+                        CustomerName = cr.Customer.Name,
+                    };
+
+        var result = await query.ToListAsync();
+
+        Print(result);
+
+        /*
+         
+      SELECT [r].[Id], [r].[Name], [c0].[Id] AS [CustomerId], [c0].[Name] AS [CustomerName]
+      FROM [Retailers] AS [r]
+      INNER JOIN [CustomerRetailers] AS [c] ON [r].[Id] = [c].[RetailerId]
+      INNER JOIN [Customers] AS [c0] ON [c].[CustomerId] = [c0].[Id]
+
+        */
+    }
+}
